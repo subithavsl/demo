fd8f7930ec49b3c8957ca4a5727ee2628ef11613
subithavsl <subithaambika@gmail.com>
2024-04-08 11:53:34 +0530

    first commit

---
+++ welcome to azure family.py
@@ -0,0 +1,12 @@
+a="welcome to azure family"
+d=len(a)
+b = a.upper()
+c = a.lower()
+slice_1 = a[1:3]
+slice_2 = a[3:8]
+slice_3 = a[6:9]
+slice_4 = a[2:5]
+slice_5 = a[3:9]
+slice_6 = a[2:3]
+slice_7 = a[7:9]
+print(slice_4)
